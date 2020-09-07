c7c4a0cc765bba2348adbf1ba31589849dd2ba6d
Brian Carrigan <brian@bcarrigan.com>
2020-09-07 18:00:45 -0400

    DMA mem-to-mem working

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,11 @@ use cortex_m_semihosting::hprintln;
 use stm32f4xx_hal::stm32::{Peripherals};
 use stm32f4xx_hal::prelude::*;
 use stm32f4xx_hal::delay::Delay;
+use stm32f4xx_hal::dma::{Transfer, config::DmaConfig, MemoryToMemory, StreamsTuple};
+use stm32f4xx_hal::dma::traits::{Direction};
+
+static mut FROM: [u32; 10] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+static mut TO: [u32; 10] = [0; 10];
 
 #[entry]
 fn main() -> ! {
@@ -27,8 +32,9 @@ fn main() -> ! {
     let cortex_p = cortex_m::Peripherals::take().unwrap();
 
     //  Get the clocks from the STM32 Reset and Clock Control (RCC) and freeze the Flash Access Control Register (ACR).
+    periph.RCC.ahb1enr.write(|w| w.dma2en().enabled());
+    
     let rcc = periph.RCC.constrain();
-    let _flash = periph.FLASH;
     let clocks = rcc.cfgr.sysclk(168.mhz()).freeze();
     let gpiod = periph.GPIOD.split();
 
@@ -37,14 +43,34 @@ fn main() -> ! {
 
     let mut delay = Delay::new(cortex_p.SYST, clocks);
 
+    let dma_config = DmaConfig::default()
+        .fifo_enable(true)
+        .memory_increment(true)
+        .peripheral_increment(true);
+
+    let mem_to_mem: MemoryToMemory<u32> = MemoryToMemory::new();
+    let streams = StreamsTuple::new(periph.DMA2);
+
+    unsafe {
+        let mut transfer = Transfer::init(
+            streams.0, 
+            mem_to_mem,
+            &mut TO,
+            Some(&mut FROM),
+            dma_config
+        );
+
+        transfer.start(|_dma_periph| ());
+    }
+
     loop {
-        led.set_high();
+        led.set_high().unwrap();
 
         //  Wait 1,000 millisec (1 sec).
         delay.delay_ms(1000_u16);
 
         //  Output 0V on the LED Pin and show a message in OpenOCD console.
-        led.set_low();
+        led.set_low().unwrap();
 
         //  Wait 1,000 millisec (1 sec).
         delay.delay_ms(1000_u16);
